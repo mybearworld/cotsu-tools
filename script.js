c18661d668317f98177a2cbfe8571831591af6e9
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-09-10 16:26:03 +0200

    1.6.6

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace   mybearworld
 // @match       *://cotsu.de/*
 // @grant       GM.xmlHttpRequest
-// @version     1.6.5
+// @version     1.6.6
 // @license     MIT
 // @author      mybearworld
 // @description Userscript für https://cotsu.de.
