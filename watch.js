33f569312f13571c57c7cbd6112d3d6306a95ec9
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-09-14 12:06:03 +0200

    Basic ESBuild setup

---
+++ watch.js
@@ -0,0 +1,23 @@
+import { context } from "esbuild";
+
+(
+  await context({
+    entryPoints: ["src/a.ts"],
+    bundle: true,
+    outfile: "out.js",
+    banner: {
+      js: `// ==UserScript==
+// @name        Cotsu-Tools
+// @namespace   mybearworld
+// @match       *://cotsu.de/*
+// @grant       GM.xmlHttpRequest
+// @version     1.7.2
+// @license     MIT
+// @author      mybearworld
+// @description Userscript für https://cotsu.de.
+// @updateURL   https://openuserjs.org/meta/mybearworld/Cotsu-Tools.meta.js
+// @downloadURL https://openuserjs.org/install/mybearworld/Cotsu-Tools.user.js
+// ==/UserScript==`,
+    },
+  })
+).watch();
