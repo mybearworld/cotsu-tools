2abcdffad9e7ee202a54354a9037b83787bf1e37
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-09-19 18:17:55 +0200

    1.9.0

--- watch.js
+++ watch.js
@@ -14,7 +14,7 @@ import { context } from "esbuild";
 // @namespace   mybearworld
 // @match       *://cotsu.de/*
 // @grant       GM.xmlHttpRequest
-// @version     1.8.1
+// @version     1.9.0
 // @license     MIT
 // @author      mybearworld
 // @description Userscript für https://cotsu.de.
