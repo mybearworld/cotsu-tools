9901dac9d7229f8b7084dc5a8f407adb486a5186
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-09-21 13:19:14 +0200

    Remove season words from definition

--- src/lib/wadokuInformation.ts
+++ src/lib/wadokuInformation.ts
@@ -94,6 +94,7 @@ export const getWadokuInformation = async (
           throw new Error("currentElement is not an element");
         if (
           currentNode.classList.contains("genus") ||
+          currentNode.classList.contains("season") ||
           currentNode.matches(".transcr:has(+ .jap)") ||
           (currentNode.classList.contains("klammer") &&
             currentNode.classList.contains("global"))
