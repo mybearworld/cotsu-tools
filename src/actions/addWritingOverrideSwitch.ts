8539496a878945118d08626fece5524fd95c98f0
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-10-03 20:25:08 +0200

    Allow switching an account with progress to reading mode

--- src/actions/addWritingOverrideSwitch.ts
+++ src/actions/addWritingOverrideSwitch.ts
@@ -31,20 +31,20 @@ export const addWritingOverrideSwitch = (records: MutationRecord[]) => {
     };
     switchButton.addEventListener("click", () => {
       if (!stats) return;
+      const name = actionName(!writingOverride());
       if (
-        stats.progress.n5.learning ||
-        stats.progress.n4.learning ||
-        stats.progress.n3.learning ||
-        stats.progress.n2.learning ||
-        stats.progress.n1.learning
+        (!stats.progress.n5.learning &&
+          !stats.progress.n4.learning &&
+          !stats.progress.n3.learning &&
+          !stats.progress.n2.learning &&
+          !stats.progress.n1.learning) ||
+        confirm(
+          `Du hast auf diesem Konto schon Fortschritt. Wenn du jetzt anfängst, zu ${name}, wird dein Fortschritt beim Lesen und beim Schreiben gemischt und eher hinderlich sein.\n\nWenn du willst, kannst du trotzdem trotzdem jetzt zum ${name[0].toUpperCase() + name.slice(1).toLowerCase()} wechseln - wenn du dich zum Beispiel gerade zum ersten Mal auf diesem Gerät auf diesem Konto anmeldet.\n\nMöchtest du das machen?`,
+        )
       ) {
-        alert(
-          `Du hast auf diesem Konto schon Fortschritt. Erstelle ein neues Konto, um auf diesem zu ${actionName(!writingOverride())}.`,
-        );
-        return;
+        toggleWritingOverride();
+        updateLabels();
       }
-      toggleWritingOverride();
-      updateLabels();
     });
     updateLabels();
     readingH3.append(" ", switchButton);
