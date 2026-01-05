141bf4377cd884d85420de3bb24a17a5cfb41dd2
mybearworld <130385691+mybearworld@users.noreply.github.com>
2026-01-05 12:08:15 +0100

    Readd accidentally removed Wadoku information in Kanji tab

---
+++ src/actions/addWadokuInformationInKanjiTab.ts
@@ -0,0 +1,47 @@
+import { element } from "../lib/element";
+import { katakanaToHiragana } from "../lib/katakanaToHiragana";
+import { pitchAccentElement, meaningElement } from "../lib/wadokuInformation";
+
+export const addWadokuInformationInKanjiTab = async (
+  records: MutationRecord[],
+) => {
+  const items = [];
+  for (const record of records) {
+    const firstAddedNode = record.addedNodes[0];
+    if (
+      record.target instanceof HTMLElement &&
+      record.target.parentElement?.parentElement?.className.startsWith(
+        "suche-module--container--",
+      ) &&
+      record.target.classList.contains("MuiList-root") &&
+      firstAddedNode instanceof HTMLElement
+    ) {
+      const text = element(
+        firstAddedNode.querySelector(
+          ".MuiListItemText-root .MuiTypography-root",
+        ),
+      );
+      if (text.dataset.cotsuToolsAddedWadokuInformation === "true") {
+        continue;
+      }
+      const match = text.textContent.match(/^(.+?)（(.+?)） (.+)?$/);
+      if (!match) throw new Error("Unexpected text format");
+      const [, kanji, kana, german] = match;
+      const reading = katakanaToHiragana(kana);
+      items.push({ kanji, reading, german, element: text });
+    }
+  }
+  if (items.length === 0) return;
+  const bulk = items.map(({ kanji }) => kanji);
+  items.forEach(({ element, kanji, reading, german }) => {
+    element.innerHTML = "";
+    element.dataset.cotsuToolsAddedWadokuInformation = "true";
+    element.append(
+      kanji,
+      "（",
+      pitchAccentElement(kanji, reading, bulk),
+      "） ",
+      german || meaningElement(kanji, reading, bulk),
+    );
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { handleExerciseWords } from "./actions/handleExerciseWords";
 import { showMoreStats } from "./actions/showMoreStats";
 import { showExercisesLeftInKanjiLearningTab } from "./actions/showExercisesLeftInKanjiLearningTab";
 import { fixFontInNewKanjiTab } from "./actions/fixFontInNewKanjiTab";
+import { addWadokuInformationInKanjiTab } from "./actions/addWadokuInformationInKanjiTab";
 import { startInterceptingFetch } from "./lib/interceptedFetch";
 import { insertCSS } from "./lib/style";
 
@@ -14,6 +15,7 @@ new MutationObserver(async (records) => {
   showMoreStats(records);
   showExercisesLeftInKanjiLearningTab(records);
   fixFontInNewKanjiTab(records);
+  addWadokuInformationInKanjiTab(records);
 }).observe(document.body, {
   childList: true,
   subtree: true,
